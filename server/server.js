11e3bea66c9ef40be41657cfc570b8a8f633882c
Nhan Dang <hoangnhan1195@gmail.com>
2020-04-26 02:41:02 -0700

    First commit

---
+++ server/server.js
@@ -0,0 +1,18 @@
+//server/server.js
+var express = require('express');
+var router = require('./routes/routes.js');
+var path = require('path');
+
+var app = express();
+
+// Define view template
+app.set('view engine', 'ejs');
+
+// Front-end reference
+app.set('views', path.join(__dirname, '../client'));
+app.use(express.static(path.join(__dirname, '../client')));
+
+// Routes reference
+app.use('/', router);
+
+module.exports=app;
