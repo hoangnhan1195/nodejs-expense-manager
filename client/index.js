52960302d6374aaca0cec560f2a996f5bec3e8f9
Nhan Dang <hoangnhan1195@gmail.com>
2020-06-21 07:19:30 -0700

    Complete up app

--- client/index.js
+++ client/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import { HashRouter } from 'react-router-dom';
+import Routes from './routes';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <HashRouter>
+    <Routes />
+  </HashRouter>, document.getElementById('root')
 );
--- server/server.js
+++ server/server.js
@@ -5,12 +5,21 @@ var path = require('path');
 
 var app = express();
 
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
 // Define view template
 app.set('view engine', 'ejs');
 
 // Front-end reference
 app.set('views', path.join(__dirname, '../client'));
 app.use(express.static(path.join(__dirname, '../client')));
+
+app.use(bodyParser.json({limit:'50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: false}));
+
+// Connect database
+mongoose.connect('mongodb://nhan:matkhau456@localhost:27017/expense-manager');
 
 // Routes reference
 app.use('/', router);
